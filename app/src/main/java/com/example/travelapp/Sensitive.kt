6034ec0bc24526b30fdb132098784ab2c534b1b4
Sudharssen Murali <sudharssen@Sudharssens-MacBook-Pro-2.local>
2025-01-14 00:39:21 -0500

    Removed api key, added env file(hidden)

--- app/src/main/java/com/example/travelapp/Sensitive.kt
+++ app/src/main/java/com/example/travelapp/Sensitive.kt
@@ -2,7 +2,7 @@ package com.example.travelapp
 
 class Sensitive {
 
-    private var apiKey: String = "FppKtToRGYjqOMPoi3GenlX01L24wIMl"
+    private var apiKey: String = 'apiKey'
 
     fun getApiKey(): String {
         return apiKey
